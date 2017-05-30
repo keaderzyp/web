84259a5b33b4977098060fbfd30b9f6efb7c6c3e
张云鹏 <zhangyunpeng@zhangyunpengdeMacBook-Pro.local>
2017-05-30 16:06:16 +0800

    test wp

---
+++ ex_wp/src/js/main.js
@@ -0,0 +1,4 @@
+require('../css/style.css');
+var $ = require('jquery');
+document.getElementById("a").innerHTML="神奇的webpack";
+ $("#a").append("<br/>我是jquery，大家好呀")
---
+++ ex_wp/src/js/vender.js
@@ -0,0 +1,2 @@
+document.getElementById("b").innerHTML="two modules are in this page";
+ 
